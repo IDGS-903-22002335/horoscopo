4b6dc9fe8308fbb8cde36a29f709f1bfa9d3a1ad
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-09-26 19:32:44 -0600

    imagen de fondo agregada

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4rglne60iag"
+    "revision": "0.bplru71r7n8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
